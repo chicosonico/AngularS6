a01c9c3a4f9b3fcf0964c7e7b8976d002ab35f1b
“chicosonico” <schvarzman@gmail.com  git config --global user.name chicosonico git config --global user.email schvarzman@gmail.com>
2023-02-15 20:57:35 +0100

    Exercici 4

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -10,9 +10,30 @@ export class EscenaComponent implements OnInit {
 
   @Input() public frases!:Frase[];
 
+  public currentSentence = 1;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public prev(){
+    if(this.currentSentence <= 1){
+      return
+    }else{
+      this.currentSentence -= 1;  
+    }
+
+  };
+
+  public next(){
+    if(this.currentSentence >= 4){
+      return
+    }else{
+      this.currentSentence += 1;
+
+    }
+
+  };
 
 }
